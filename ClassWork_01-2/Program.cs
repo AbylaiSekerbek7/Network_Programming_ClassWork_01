932975a816a721fdf4af4506d4915237871fd71b
Секербек Абылайхан Кайратулы <SekerbekA@lcoast.itstep>
2024-04-05 21:18:53 +0500

    Server - Client work, new changes

---
+++ ClassWork_01-2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Libraries
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+
+// Client Programm
+namespace ClassWork_01_2
+{
+    internal class Program
+    {
+        static Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter server ip: ");
+            string IpServer = Console.ReadLine();
+            Console.Write("Enter server port");
+            int port = int.Parse(Console.ReadLine());
+
+            try
+            {
+                // 1) Client - Server connection
+                client.Connect(IpServer, port);
+                
+                // 2) Check connection
+                if (client.Connected == false)
+                {
+                    throw new Exception("No connection with server");
+                }
+                Console.WriteLine("Success - Connected with server");
+                
+                // 3) Server ==> "Hello"
+                byte[] buffer = new byte[1024];
+                int size = client.Receive(buffer);
+                Console.WriteLine($"Message from server [{size}]: {Encoding.UTF8.GetString(buffer, 0, size)}");
+                
+                // 4) UserName ==> Server
+                string UserName = "Abylaikhan";
+                client.Send(Encoding.UTF8.GetBytes(UserName));
+                Console.WriteLine($"Name {UserName} send to server");
+
+                // 5) 
+                while (true)
+                {
+                    // 5.1)
+                    Console.WriteLine("Enter messages to server");
+                    string msg = Console.ReadLine();
+                    if (msg == "") { continue; }
+                    client.Send(Encoding.UTF8.GetBytes(msg));
+                    // 5.2)
+                    size = client.Receive(buffer);
+                    string temp = Encoding.UTF8.GetString(buffer, 0, size);
+                    Console.WriteLine($"Server message[{size}]: {temp}");
+
+                    if (msg == "Exit")
+                    {
+                        size = client.Receive(buffer);
+                        temp = Encoding.UTF8.GetString(buffer, 0, size);
+                        Console.WriteLine($"Server message[{size}]: {temp}");
+                        client.Disconnect(true); // Disconnect boolean true/false answers
+                                                 // For this question, "Использовать ли сокет повторно"
+                                                 // True - Да, не уничтожать
+                                                 // False - Нет, разорвать связь тоже самое что и client.Close();
+                        client.Close();
+                        break;
+                    }
+                } // while (true)
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error - {ex.Message}");
+            } // try-catch
+            Console.WriteLine("Client exit...");
+            Console.ReadLine();
+
+        } // static void Main()
+    } // internal class Program
+} // namespace ClassWork_01_2
--- ClassWork_01/Program.cs
+++ ClassWork_01/Program.cs
@@ -3,13 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// Libraries
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
+
+
+// Server Programm
 namespace ClassWork_01
 {
     internal class Program
     {
+        // most popular protocols IP/TCP = IP - "InterNetwork", TCP - "Tcp";
+        static Socket socServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        static string ipAddress = "127.0.0.1"; // localchost, local Machine
+                                               // 0.0.0.0 - Маска для всех компьютеров
+                                               // 192.168.1.10 - Локальная сеть
+        static int port = 12345; // server port, for connection
         static void Main(string[] args)
         {
-        }
-    }
-}
+            // Конечная точка сервера
+            EndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+            // Привязка сервера с EndPoint-ом
+            socServer.Bind(serverEndPoint);
+            Console.WriteLine($"Bind: {serverEndPoint}");
+            // Запускаем сокет на прослушивание сети для подключения клиента
+            // 100 - Глубина очереди ожидаемых клиентов
+            socServer.Listen(100);
+            Console.WriteLine("Listening started - waitining Client");
+
+            // 1) Accept - Ожидание подключения клиента
+            Socket client = socServer.Accept(); // в client - Будет наш клиент
+            Console.WriteLine($"Client is connected: {client.RemoteEndPoint}");
+            
+            // 2) Hello Client
+            // Кодировка
+            // UTF8 - обычно 1 символ = 1 byte
+            // Unicode - обычно 1 символ = 2 byte
+            // ASCII - лучше всего работает с En, Ru языками. Поддержку с Kz нету, мест нету
+            //byte[] buffer = Encoding.UTF8.GetBytes(ipAddress);
+            client.Send(Encoding.UTF8.GetBytes(ipAddress));
+
+            // 3) Receive message from client - имя клиента
+            byte[] bufferReceive = new byte[1024]; // buffer to get message
+            // if size = 0 Разорвана сеть
+            // if size < 0 Что то случилось - Ошибка - Исключение
+            // if size > 0 && size <= 1024 
+            int size = client.Receive(bufferReceive); // size of the message 
+            string msg = Encoding.UTF8.GetString(bufferReceive, 0, size);
+
+            Console.WriteLine($"Message Accepted! size: {size} byte");
+            Console.WriteLine($"Client Message: {msg}");
+
+            // 4) 
+            while (true)
+            {
+                // 4.1)
+                size = client.Receive(bufferReceive);
+                msg = Encoding.UTF8.GetString(bufferReceive, 0, size);
+                Console.WriteLine($"Message Accepted! size: {size} byte");
+                Console.WriteLine($"Message: {msg}");
+                // 4.2) Подтверждение от сервера о получении сообщения
+                client.Send(Encoding.UTF8.GetBytes("OK"));
+                if (msg == "Exit")
+                {
+                    client.Send(Encoding.UTF8.GetBytes("Good bye client...."));
+                    client.Close(); // stop connection with client
+                    break;
+                }
+            } // while (true)
+
+            Console.WriteLine("Server exit...");
+            Console.ReadLine();
+        } // static void Main(string[] args)
+    } // internal class Programm
+} // namespace ClassWork_01
